ff196ed6c3dc68601118858311c1fbdc4d132ddb
Amandeep Singh <nexaxe21@gmail.com>
2021-02-03 08:08:14 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -16,6 +16,17 @@ def button_click(number):
 def button_clear():
     e.delete(0,END)
 
+
+def button_add():
+    first_number=e.get()
+    global f_num 
+    f_num = int(first_number)
+    e.delete(0,END)
+
+def button_equal():
+    second_number=e.get()
+    e.delete(0,END)
+    e.insert(0,f_num+int(second_number))
 #define buttons
 
 myButton_1 = Button(root, text="1",padx=40,pady=20,command=lambda: button_click(1))
@@ -29,10 +40,10 @@ myButton_8 = Button(root, text="8",padx=40,pady=20,command=lambda: button_click(
 myButton_9 = Button(root, text="9",padx=40,pady=20,command=lambda: button_click(9))
 myButton_0 = Button(root, text="0",padx=40,pady=20,command=lambda: button_click(0))
 
-myButton_add = Button(root, text="+",padx=39,pady=20,command=button_click)
+myButton_add = Button(root, text="+",padx=39,pady=20,command=button_add)
 
-myButton_clear = Button(root, text="Clear",padx=79,pady=20,command=lambda: button_clear())
-myButton_equals = Button(root, text="=",padx=91,pady=20,command=lambda: button_click())
+myButton_clear = Button(root, text="Clear",padx=79,pady=20,command=button_clear)
+myButton_equals = Button(root, text="=",padx=91,pady=20,command=button_equal)
 
 #put buttons on the screen
 
